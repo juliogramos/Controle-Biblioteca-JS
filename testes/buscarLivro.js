6f5341bad1f1634e2dd9024631e94cb8a35b6498
Julio <juliogoncalvesramos@yahoo.com.br>
2026-08-06 17:57:37 -0300

    testa buscarLivro

---
+++ testes/buscarLivro.js
@@ -0,0 +1,7 @@
+import { buscarLivro } from "../buscarLivro.js";
+
+// Tentando buscar o último livro, React para Web
+buscarLivro("react para web");
+
+// Tentando buscar livro que não existe
+buscarLivro("Livro que não existe");
